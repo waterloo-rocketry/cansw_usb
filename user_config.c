6a81eff99d8dfe429a4607e4d77af5ee1d7c1c2f
Aaron Morrison <akmorris@edu.uwaterloo.ca>
2019-02-04 23:00:07 -0500

    Create user_config.c/h, and unit tests too
    All 8 unit tests currently fail. Modify user_config.c until they
    pass. Yay test driven development!
    
    user_config is going to be the library that takes strings from the
    user over usb and then modifies the configuration, and what the board
    prints out when it receives CAN messages

---
+++ user_config.c
@@ -0,0 +1,18 @@
+#include "user_config.h"
+
+void parse_usb_string(const char *input)
+{
+    // Add code here.
+}
+
+uint8_t max_debug_level()
+{
+    // And here
+    return 0;
+}
+
+bool allow_sensor_messages()
+{
+    // And here
+    return false;
+}
