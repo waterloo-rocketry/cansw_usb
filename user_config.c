5d57ba582340dc54c3c7d8b4d34564cf4a2f87b9
Dawson Kletke <dawsonwkletke@gmail.com>
2019-03-26 12:45:31 -0400

    Addressed more requested changes: -Used snprintf to save the size of the message (and be used for usb_app_write_string) -Used snprintf instead of sprintf to limit the size of the message -Used "\r\n" instead of "\n" -Added check for if input message pointer was not equal to NULL --> then let input message go through function 	-If input message is false, exit function early

--- user_config.c
+++ user_config.c
@@ -19,60 +19,66 @@ static char save_type;
 
 // function takes in a message and parses it to see if max_debug_level() or allow_sensor_messages() needs to be changed
 void parse_usb_string(const char *input) {
-	for (uint8_t i = 0; input[i] != '\0'; i++) {
-		switch (check_level) {
-		// Case 2: last check for if there has been a valid message sent
-		// --> if parse_usb_string has received a semicolon
-		case CHECK_SEMI_COLON:
-			check_level = CHECK_CHAR;
-			if (input[i] == ';') {
-				switch (save_type) {
+	// Check for if the input is not NULL, if so: continue parsing the message
+	// Else: end function early
+	if (input != NULL) {
+		for (uint8_t i = 0; input[i] != '\0'; i++) {
+			switch (check_level) {
+			// Case 2: last check for if there has been a valid message sent
+			// --> if parse_usb_string has received a semicolon
+			case CHECK_SEMI_COLON:
+				check_level = CHECK_CHAR;
+				if (input[i] == ';') {
+					switch (save_type) {
+					case 'G':
+						set_debug_level = temp_num;
+						break;
+					case 'S':
+						set_sensor_messages = temp_num;
+						break;
+					case 'L':
+						;
+						char config_msg[72];
+						char sensor_check[8];
+						uint8_t msg_length = 0;
+						strcpy(sensor_check, "are");
+						if (!allow_sensor_messages())
+							strcat(sensor_check, " not");
+						msg_length = snprintf(config_msg, 72, "Current Config: Max debug level = %d & Sensor messages %s allowed!", max_debug_level(), sensor_check);
+						config_msg[71] = '\r\n';
+						config_msg[72] = '\0';
+						usb_app_write_string(config_msg, msg_length);
+						break;
+					}
+				} // @suppress("No break at end of case")
+
+				// Case 1: second check for if there has been a valid message sent
+				// --> if parse_usb_string has received a valid number with respect to the char previously received
+			case CHECK_NUM:
+				temp_num = input[i] - '0';
+				if ((temp_num >= 0 && temp_num <= 5 && save_type == 'G') || ((temp_num == 0 || temp_num == 1) && save_type == 'S')) {
+					check_level = CHECK_SEMI_COLON;
+					break;
+				} else
+					check_level = CHECK_CHAR; // @suppress("No break at end of case")
+
+				// Case 0: new instance of checking for valid message sent
+				// --> if parse_usb_string has received a char (either 'S' or 'G')
+			case CHECK_CHAR:
+				switch (input[i]) {
 				case 'G':
-					set_debug_level = temp_num;
+					save_type = 'G';
+					check_level = CHECK_NUM;
 					break;
 				case 'S':
-					set_sensor_messages = temp_num;
+					save_type = 'S';
+					check_level = CHECK_NUM;
 					break;
-				case 'L': ;
-					char config_msg[72];
-					char sensor_check[8];
-					strcpy(sensor_check, "are");
-					if (!allow_sensor_messages())
-						strcat(sensor_check, " not");
-					sprintf(config_msg, "Current Config: Max debug level = %d & Sensor messages %s allowed!", max_debug_level(), sensor_check);
-					config_msg[71] = '\n';
-					config_msg[72] = '\0';
-					usb_app_write_string(config_msg, 72);
+				case 'L':
+					save_type = 'L';
+					check_level = CHECK_SEMI_COLON;
 					break;
 				}
-			} // @suppress("No break at end of case")
-
-			// Case 1: second check for if there has been a valid message sent
-			// --> if parse_usb_string has received a valid number with respect to the char previously received
-		case CHECK_NUM:
-			temp_num = input[i] - '0';
-			if ((temp_num >= 0 && temp_num <= 5 && save_type == 'G') || ((temp_num == 0 || temp_num == 1) && save_type == 'S')) {
-				check_level = CHECK_SEMI_COLON;
-				break;
-			} else
-				check_level = CHECK_CHAR; // @suppress("No break at end of case")
-
-			// Case 0: new instance of checking for valid message sent
-			// --> if parse_usb_string has received a char (either 'S' or 'G')
-		case CHECK_CHAR:
-			switch (input[i]) {
-			case 'G':
-				save_type = 'G';
-				check_level = CHECK_NUM;
-				break;
-			case 'S':
-				save_type = 'S';
-				check_level = CHECK_NUM;
-				break;
-			case 'L':
-				save_type = 'L';
-				check_level = CHECK_SEMI_COLON;
-				break;
 			}
 		}
 	}
