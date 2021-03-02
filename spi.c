7aa34851b1f862b9f1ae5b87c6cbf0928a5cd615
Jacob Deery <jacob.deery@gmail.com>
2021-03-01 19:29:27 -0500

    Add astyle formatting script (#10)
    This commit adds a format.sh script that can be used to autoformat code
    using AStyle. A default .astylerc is additionally provided.

--- spi.c
+++ spi.c
@@ -2,7 +2,8 @@
 #include <stdint.h>
 #include <xc.h>
 
-void spi_init() {
+void spi_init()
+{
     //mark SCL as output
     TRISCbits.TRISC0 = 0;
     //mark MISO as input
@@ -16,29 +17,32 @@ void spi_init() {
     SSPSTAT = 0b10000000;
     SSPCON1 = 0b00110010;
     SSPCON2 = 0b00000000;
-    
+
 }
 
-void spi_write(uint8_t data) {
+void spi_write(uint8_t data)
+{
     uint8_t temp = SSPBUF;
     PIR1bits.SSP1IF = 0;
     SSPCONbits.WCOL = 0;
     SSPBUF = data;
-    while(!PIR1bits.SSP1IF) {};
+    while (!PIR1bits.SSP1IF) {};
 }
 
-uint8_t spi_read(void) {
+uint8_t spi_read(void)
+{
     //read the SSPBUF so it doesn't yell at you
     uint8_t temp = SSPBUF;
     PIR1bits.SSP1IF = 0;
     //clock 0 onto the pins
     SSPBUF = 0;
-    while(!PIR1bits.SSP1IF) {}
+    while (!PIR1bits.SSP1IF) {}
     return SSPBUF;
 }
 
-void cs_drive(uint8_t state) {
-    if(state)
+void cs_drive(uint8_t state)
+{
+    if (state)
         LATC |= (1 << 3);
     else
         LATC &= ~(1 << 3);
