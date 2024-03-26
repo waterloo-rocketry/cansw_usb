a018c6619f345640676874145b0aef8f6e39a187
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2024-03-25 23:28:41 -0400

    Prevent output period character when received duplicate CAN messages

--- usb_app.c
+++ usb_app.c
@@ -35,26 +35,9 @@ void usb_app_heartbeat(void) {
     CDCTxService();
 }
 
-// Compares 2 messages (and therefore all of their elements)
-bool compare_can_msg(const can_msg_t *msg1, const can_msg_t *msg2) {
-    if (msg1->data_len != msg2->data_len) {
-        return false;
-    }
-    for (int i = 0; i < msg1->data_len; i++) {
-        if (msg1->data[i] != msg2->data[i]) {
-            return false;
-        }
-    }
-    if (msg1->sid != msg2->sid) {
-        return false;
-    }
-    return true;
-}
-
 uint8_t usb_app_report_can_msg(const can_msg_t *msg) {
     // length for 1 byte ("XX,") * number of bytes-1 + extras and last byte
     char temp_buffer[3 * 7 + 10];
-    static can_msg_t last_received_message;
     const char hex_lookup_table[16] = {
         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
@@ -74,38 +57,29 @@ uint8_t usb_app_report_can_msg(const can_msg_t *msg) {
         return 0;
     }
 
-    // Checks if the last message is the same as the new message coming in
-    // If true, prints a dot and ends the function
-    // Otherwise, prints the message normally
-    if (compare_can_msg(msg, &last_received_message)) {
-        temp_buffer[0] = '.';
-        temp_buffer[1] = '\0';
-    } else {
-        last_received_message = *msg;
-        // temp_buffer is the character array of the message that gets printed
-        // out when usb_app_report_can_msg is called on.
-        // The first character is to identify this as a valid message
-        // The next 3 characters are the sid of the input message followed by a
-        // ':' The next 2 characters are elements of the data array apart of the
-        // input message followed by a ',' The next 2 characters are for
-        // formatting output The last character is used to end the string made
-        // from temp_buffer
-        temp_buffer[0] = '$';
-        temp_buffer[1] = hex_lookup_table[(msg->sid >> 8) & 0xf];
-        temp_buffer[2] = hex_lookup_table[(msg->sid >> 4) & 0xf];
-        temp_buffer[3] = hex_lookup_table[msg->sid & 0xf];
-        temp_buffer[4] = ':';
-        uint8_t i;
-        for (i = 0; i < msg->data_len && i < 8; ++i) {
-            temp_buffer[3 * i + 5] = hex_lookup_table[(msg->data[i] >> 4)];
-            temp_buffer[3 * i + 6] = hex_lookup_table[(msg->data[i] & 0xf)];
-            temp_buffer[3 * i + 7] = ',';
-        }
-        i -= 1;
-        temp_buffer[3 * i + 7] = '\n';
-        temp_buffer[3 * i + 8] = '\r';
-        temp_buffer[3 * i + 9] = '\0';
+    // temp_buffer is the character array of the message that gets printed
+    // out when usb_app_report_can_msg is called on.
+    // The first character is to identify this as a valid message
+    // The next 3 characters are the sid of the input message followed by a
+    // ':' The next 2 characters are elements of the data array apart of the
+    // input message followed by a ',' The next 2 characters are for
+    // formatting output The last character is used to end the string made
+    // from temp_buffer
+    temp_buffer[0] = '$';
+    temp_buffer[1] = hex_lookup_table[(msg->sid >> 8) & 0xf];
+    temp_buffer[2] = hex_lookup_table[(msg->sid >> 4) & 0xf];
+    temp_buffer[3] = hex_lookup_table[msg->sid & 0xf];
+    temp_buffer[4] = ':';
+    uint8_t i;
+    for (i = 0; i < msg->data_len && i < 8; ++i) {
+        temp_buffer[3 * i + 5] = hex_lookup_table[(msg->data[i] >> 4)];
+        temp_buffer[3 * i + 6] = hex_lookup_table[(msg->data[i] & 0xf)];
+        temp_buffer[3 * i + 7] = ',';
     }
+    i -= 1;
+    temp_buffer[3 * i + 7] = '\n';
+    temp_buffer[3 * i + 8] = '\r';
+    temp_buffer[3 * i + 9] = '\0';
 
     // Writes temp_buffer to a string
     if (usb_app_write_string(temp_buffer, strlen(temp_buffer))) {
